5ae37adb43bb702bd2e3a2e735699a534ca290db
kateramckay <kateradmckay@gmail.com>
2023-01-27 08:22:56 -0500

    adding function

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ const input = document.querySelector("myInput");
 const button = document.querySelector("mainButton");
 const listItem = document.querySelector("mylist");
 
-button.addEventListener('click', addingListItems)
+button.addEventListener('click', addingListItems);
+
+const addingListItems = function() {
+
+
+}
