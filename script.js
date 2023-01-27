38a0fd274fae9ddac0d74e665fbe84a0cc7bc456
kateramckay <kateradmckay@gmail.com>
2023-01-27 09:51:08 -0500

    adding more code

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const listItem = document.querySelector("mylist");
 button.addEventListener('click', addingListItems);
 
 const addingListItems = function() {
-
+ const element = document.createElement("li");
+ element.class = "list-item"
+ element.textContent = input.value;
 
 };
+
